53be2819270677d672c755eb03fb0d03e912e3ff
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-06 21:00:30 +0000

    Added paper and scissor paths. Added links that appear at the top of every path

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,59 @@
+
+class ZebraController < ApplicationController
+
+  # the methods name has to be the name of the action
+  def giraffe
+
+    moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"  
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won" 
+    
+  end
+  render({ :template => "game_templates/play_rock" })
+  end
+
+  def lion
+    moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+  render({ :template => "game_templates/play_paper" })
+
+  end
+
+  def tiger
+    moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
+  render({ :template => "game_templates/play_scissor" })
+
+  end
+
+  def rules
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
 
   get("/home", { :controller =>"pages", :action =>"home" })
-end
 
-# "/home" is like the path
+  get("/rock", { :controller =>"zebra", :action =>"giraffe"})
+
+  get("/paper", { :controller =>"zebra", :action =>"lion"})
+
+  get("/scissors", { :controller =>"zebra", :action =>"tiger"})
+
+  get("/", { :controller =>"zebra", :action =>"rules"})
+
+
+  
+  
+end
+# this file is equivalent to app.rb
+# "/home" is like the path name
 # controller is where all the ruby code logic goes
 
 # action is the html view template
+
+# you can use the same controller but different actions/methods
