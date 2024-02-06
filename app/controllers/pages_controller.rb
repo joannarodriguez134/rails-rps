612d994c04c6dc6ca259c9fb6cdbd13b95d69afa
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-06 19:41:15 +0000

    Started to add the home path

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+
+class PagesController < ApplicationController
+  def home
+  @date = Date.today.day
+  render({ :template = "pages_templates/home" }) 
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  get("/home", { :controller ="pages", :action = "home" })
 end
+
+# "/home" is like the path
+# controller is where all the ruby code works
+
+# action is the html view template
